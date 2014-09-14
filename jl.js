e9e32f499184fda3eaa9492c4a338b525ebc4bde
Alessandro Roat <alexroat@gmail.com>
2014-09-14 10:50:12 +0200

    Update jl.js

--- jl.js
+++ jl.js
@@ -10,7 +10,11 @@ function jl(e,layout)
 	//read layout
 	var ll=jl.getLayout(e);
 	if (jl.fn[ll.sz])
+    {
+        var st={"overflow":"hidden"};
+        jl.setStyle(e,st);
 		jl.fn[ll.sz](e);
+    }
 	//children process
 	for (var i=0;i<e.children.length;i++)
     {
@@ -203,7 +207,7 @@ jl.fn.fitme=function(e){
 jl.fn.box=function(e){
     var ll=jl.getLayout(e);
     ll.dir=ll.dir=='v'?'v':'h';
-	var ts={'h':e.offsetWidth,'v':e.offsetHeight}[ll.dir];
+	var ts=jl.getSizes(e)[{'h':"width",'v':"height"}[ll.dir]];
 	var tp=0;
     var cc=jl.children(e);
 	for (var i=0;i<cc.length;i++)
@@ -290,7 +294,6 @@ jl.fn.tabs=function(e){
     if (!e.layout)
 	{
    		e.layout={};
-        jl.setStyle(e,{"overflow":"hidden"});
         e.layout.flaps=[];
         e.layout.cc=jl.children(e);
         var header=document.createElement("div");
@@ -338,7 +341,6 @@ jl.fn.flaps=function(e){
     if (!e.layout)
 	{
    		e.layout={};
-        jl.setStyle(e,{"overflow":"hidden"});
         e.layout.flaps=[];
         e.layout.cc=jl.children(e);
         var header=document.createElement("div");
@@ -361,7 +363,8 @@ jl.fn.flaps=function(e){
                 jl.setStyle(flap,{display:"inline-block"});
             flap.addEventListener("click",(function (i){return function(){ll.sel=(ll.sel==i)?null:i;jl.setLayout(e,ll);jl(e);jl(e.parentNode);};})(i));
         }
-
+        //schedula la rivalidazione del padre
+        setTimeout(function(){jl(e.parentNode);},0);
     }
     
     var hs=jl.getSizes(e.layout.header);
@@ -395,7 +398,6 @@ jl.fn.sequence=function(e){
     if (!e.layout)
 	{
    		e.layout={};
-        jl.setStyle(e,{"overflow":"hidden"});
         e.layout.prev=document.createElement("span");
         e.layout.prev.textContent="<";
         jl.toggleClass(e.layout.prev,"jlexclude");
